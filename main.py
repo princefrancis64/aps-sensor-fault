bc83a630b9ccebd804f426c39c988698812c24b1
princefrancis64 <prince.francis64@gmail.com>
2023-12-03 13:54:23 +0530

    completed data validation

--- main.py
+++ main.py
@@ -1,24 +1,26 @@
-import pymongo
+from sensor.logger import logging
+from sensor.exception import SensorException
+from sensor.utils import get_collection_as_dataframe
+import sys,os
+from sensor.entity import config_entity
+from sensor.components.data_ingestion import DataIngestion
+from sensor.components.data_validation import DataValidation
 
-# Provide the mongodb localhost url to connect python to mongodb.
-client = pymongo.MongoClient("mongodb://localhost:27017/neurolabDB")
-# Database Name
-dataBase = client["neurolabDB"]
 
-# Collection  Name
-collection = dataBase['Products']
+if __name__=="__main__":
+     try:
+          training_pipeline_config = config_entity.TrainingPipelineConfig()
 
-# Sample data
-d = {'companyName': 'iNeuron',
-     'product': 'Affordable AI',
-     'courseOffered': 'Machine Learning with Deployment'}
+          #data ingestion
+          data_ingestion_config = config_entity.DataIngestionConfig(training_pipeline_config = training_pipeline_config)
+          print(data_ingestion_config.to_dict())
+          data_ingestion = DataIngestion(data_ingestion_config= data_ingestion_config)
+          data_ingestion_artifact = data_ingestion.initiate_data_ingestion()
 
-# Insert above records in the collection
-rec = collection.insert_one(d)
+          #data validation
+          data_validation_config = config_entity.DataValidationConfig(training_pipeline_config=training_pipeline_config)
+          data_validation = DataValidation(data_validation_config = data_validation_config,data_ingestion_artifact = data_ingestion_artifact)
+          data_validation_artifact = data_validation.initiate_data_validation()
+     except Exception as e:
+          print(e)
 
-# Lets Verify all the record at once present in the record with all the fields
-all_record = collection.find()
-
-# Printing all records present in the collection
-for idx, record in enumerate(all_record):
-     print(f"{idx}: {record}")
